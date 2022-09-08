6aa130bd3a4d760a21a7ff489dfbaf5bf2e47a5b
Alejandro Estrada <estradasilva@mines.edu>
2022-09-08 12:32:47 -0600

    Added hey statement

--- src/gitIntro/HelloGit.java
+++ src/gitIntro/HelloGit.java
@@ -5,5 +5,6 @@ public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
 		System.out.println("It's so good to know you!");
+		System.out.println("hey");
 	}
 }
